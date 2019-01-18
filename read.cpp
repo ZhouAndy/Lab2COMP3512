5a56b4acba52a25f15dec8da4cad8fedd8fafda2
Andy Zhou <azhou34@my.bcit.ca>
2019-01-18 09:48:20 -0800

    added additional logic for reading the file as well as sorting and performing the calculations.
    added comments to functions.

--- read.cpp
+++ read.cpp
@@ -7,6 +7,7 @@
 #include <sstream>
 #include <string>
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
@@ -15,16 +16,42 @@ void read() {
     ifstream file;
     file.open("../Readings.txt");
 
+    //Determine number of lines in the file
     int lineCount = 0;
     string line;
-
     while (getline(file, line)) {
         lineCount++;
     }
 
-    
-
     file.close();
+    //Store doubles into an array.
+    double doubleValues[lineCount];
+    double doubleValue;
+    int i = 0;
+    ifstream secondStream;
+    secondStream.open("../Readings.txt");
+    while(secondStream >> doubleValue) {
+        secondStream >> doubleValue;
+        doubleValues[i] = doubleValue;
+        i++;
+    }
+    secondStream.close();
+    //Sort the array
+    sort(doubleValues, doubleValues + lineCount);
 
+    //Perform calculations;
+    double sum = 0;
+    for (int j = 0; j < lineCount; j++) {
+        sum += doubleValues[j];
+    }
 
+    double median = lineCount % 2 != 0 ? doubleValues[lineCount / 2] :
+                    (doubleValues[lineCount / 2 - 1] + doubleValues[lineCount / 2]) / 2;
+
+    double average = sum/lineCount;
+    cout << "There are " << lineCount << " readings in the file" << endl;
+    cout << "The average reading is " << average << endl;
+    cout << "The highest reading is " << doubleValues[lineCount - 1] << endl;
+    cout << "The lowest reading is " << doubleValues[0] << endl;
+    cout << "The median reading is " << median << endl;
 };
